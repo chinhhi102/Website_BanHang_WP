3d5cc1c8868527fec27c5320e25b3467de8533dc
Cong Chinh <chinhhi102@gmail.com>
2019-07-22 17:41:39 +0700

    Create componet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './Views/User_manager/login/login.component';
+import { SignupComponent } from './Views/User_manager/signup/signup.component';
+import { ForgotPwComponent } from './Views/User_manager/forgot-pw/forgot-pw.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'forgot', component: ForgotPwComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Views/User_manager/login/login.component';
+import { ForgotPwComponent } from './Views/User_manager/forgot-pw/forgot-pw.component';
+import { SignupComponent } from './Views/User_manager/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ForgotPwComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
